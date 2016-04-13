f9bad6f7ef6539104b64aea1acd8d1ef2525c2e3
Verusss <226327@student.pwr.edu.pl>
2016-04-13 15:44:42 +0200

    Update Slav.cpp

--- Slav.cpp
+++ Slav.cpp
@@ -1,5 +1,9 @@
 #include "Slav.h"
 #include <fstream>
+#include <time.h>
+#include <stdlib.h>
+#include <iterator>
+#include <string>
 
 using namespace std;
 
@@ -18,7 +22,9 @@ void Slav::init()
 
 Slav::Slav()
 {
-	static int amountOfNames = (init(), names.size());
+	static int amountOfNames = (init(), names.size()); 
+	/*Jest to leniwa inicjalizacja. Najpierw wywołuje się funkcja init(), 
+	dzięki czemu names.size() przyjmuje wartość powstałą w wyniku działania funkcji init() zamiast 0.*/
 	_name = names[rand() % amountOfNames];
 	_id = _counter++;
 }
@@ -26,4 +32,12 @@ Slav::Slav()
 string Slav::description()
 {
 	return string("  ") + _name + " [" + to_string(_id) + "]";
+}
+
+Slav::enum_sex Slav::sex()
+{
+	if (_name.back()=='a')
+		return female;
+	else
+		return male;
 }
